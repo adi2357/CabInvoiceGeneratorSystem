f074d9151127cb9d4b083fe16c3ff58d44309882
Aditya Verma <aaadarths2357@gmail.com>
2020-10-19 18:06:50 +0530

    Added: code to get minimum fare when the calculated fare is lessthan the minimum fare and test case to check the functionality

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -3,9 +3,10 @@ package com.bridgelabz.cabinvoicegenerator;
 public class CabInvoiceGenerator {
 	private static final double COST_PER_KILOMETER = 10.0;
 	private static final int COST_PER_MINUTE = 1;
+	private static final double MINIMUM_RIDE_FARE = 5.0;
 
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
-		return totalFare;
+		return Math.max(MINIMUM_RIDE_FARE, totalFare);
 	}
 }
--- src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceServiceTest.java
@@ -13,4 +13,13 @@ public class CabInvoiceServiceTest {
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(60.0, fare, 0.0);
 	}
+
+	@Test
+	public void givenDistanceAndTime_WhenFareLessThanMinimumFare_ShouldReturnMinimumFare() {
+		CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+		double distance = 0.3;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5.0, fare, 0.0);
+	}
 }
